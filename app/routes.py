1267a2de7484a62456a97e752073c1373bbe0e6e
starseed2021 <tiff.harper18@gmail.com>
2021-10-14 21:42:02 -0400

    "added function for GET requesting one book"

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,9 @@ class Book:
         self.description = description
 
 books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+    Book(1, "Dune", "A fantasy novel set in an imaginary world."),
+    Book(2, "Hitchikers Guide to the Galaxy", "A fantasy novel set in an imaginary world."),
+    Book(3, "The Fifth Season", "A fantasy novel set in an imaginary world.")
 ]
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
@@ -27,6 +27,18 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+            
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
